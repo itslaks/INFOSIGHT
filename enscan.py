118cf9dd8cd5a82cdb850fb095c54dca72a63e3b
itslaks <sj.lakshan2004@gmail.com>
2024-08-14 16:38:20 +0530

    NEW

--- enscan.py
+++ enscan.py
@@ -106,6 +106,6 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    socket.setdefaulttimeout(10)
+    socket.setdefaulttimeout(100)
     app = create_app()
     app.run(debug=True, host='0.0.0.0', port=5000)
